e78b132cb61171a9fe4dbe4fb445e09a6f50bebd
navami-pr <navamipr@gmail.com>
2018-09-10 14:22:50 +0530

    code updated

--- app/api/image_uploader/v1/home/uploads.rb
+++ app/api/image_uploader/v1/home/uploads.rb
@@ -14,12 +14,12 @@ class ImageUploader::V1::Home::Uploads < Grape::API
         [422, 'Unprocessable Entity']
     ] do
       file_name = "#{params[:image][:filename]}_#{Time.now.to_i}"
-      Dir.mkdir('/home/navami/' + 'images') unless File.directory?('/home/navami/images')
+      Dir.mkdir('/home/' + 'images') unless File.directory?('/home/images')
       ext = File.extname(params[:image][:filename])
       binding.pry
       if %w( .jpg .jpeg .png).include? ext.downcase
-        new_file = File.open("/home/navami/images/#{file_name}", "wb") { |f| f.write(params[:image][:tempfile].read) }
-        if (FastImage.size("/home/navami/images/#{file_name}")[0].between?(350,5000))
+        new_file = File.open("/home/images/#{file_name}", "wb") { |f| f.write(params[:image][:tempfile].read) }
+        if (FastImage.size("/home/images/#{file_name}")[0].between?(350,5000))
           is_image_uploaded = true 
         else
           warning = "Files minimum of 350x350 and a maximum of 5000x5000 size"
